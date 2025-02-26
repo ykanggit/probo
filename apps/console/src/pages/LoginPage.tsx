a63fa528e77d52d6595d206dd799e1d16e7d4391
gearnode <bryan@frimin.fr>
2025-02-26 13:23:29 +0100

    Remove unused links
    Signed-off-by: gearnode <bryan@frimin.fr>

--- apps/console/src/pages/LoginPage.tsx
+++ apps/console/src/pages/LoginPage.tsx
@@ -109,24 +109,6 @@ export default function LoginPage() {
             </Button>
           </div>
         </form>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-muted/40 px-2 text-muted-foreground">
-              Or continue with
-            </span>
-          </div>
-        </div>
-        <div className="flex flex-col space-y-2">
-          <Button variant="outline" type="button" disabled={isLoading}>
-            GitHub
-          </Button>
-          <Button variant="outline" type="button" disabled={isLoading}>
-            Google
-          </Button>
-        </div>
       </div>
       <div className="text-center text-sm">
         Don&apos;t have an account?{" "}
