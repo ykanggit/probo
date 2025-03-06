b539abf5f07bb4b3f99f321da5857bb3e1c6cac6
gearnode <bryan@frimin.fr>
2025-03-06 10:20:13 +0100

    Fix migration error
    Signed-off-by: gearnode <bryan@frimin.fr>

--- pkg/usrmgr/coredata/migrations/20250225T153220Z.sql
+++ pkg/usrmgr/coredata/migrations/20250225T153220Z.sql
@@ -1,5 +1,3 @@
--- Add organization_id column to usrmgr_users table
-ALTER TABLE usrmgr_users ADD COLUMN organization_id TEXT REFERENCES organizations(id);
+ALTER TABLE usrmgr_users ADD COLUMN organization_id TEXT;
 
--- Create an index for faster lookups
 CREATE INDEX usrmgr_users_organization_id_idx ON usrmgr_users(organization_id); 
