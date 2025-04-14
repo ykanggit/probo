b3919aa8c1b84af1e407c928a3a512fa07ac9245
gearnode <bryan@frimin.fr>
2025-04-14 16:37:40 -0700

    Fix stale framework controls
    Signed-off-by: gearnode <bryan@frimin.fr>

---
+++ pkg/coredata/migrations/20250414T145300Z.sql
@@ -0,0 +1,22 @@
+DELETE FROM controls WHERE framework_id NOT IN (SELECT id FROM frameworks);
+ALTER TABLE controls DROP CONSTRAINT IF EXISTS controls_framework_id_fkey;
+
+ALTER TABLE controls
+    ADD CONSTRAINT controls_framework_id_fkey
+    FOREIGN KEY (framework_id)
+    REFERENCES frameworks(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE controls_mesures DROP CONSTRAINT IF EXISTS controls_mesures_control_id_fkey;
+ALTER TABLE controls_mesures
+    ADD CONSTRAINT controls_mesures_control_id_fkey
+    FOREIGN KEY (control_id)
+    REFERENCES controls(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE controls_policies DROP CONSTRAINT IF EXISTS controls_policies_control_id_fkey;
+ALTER TABLE controls_policies
+    ADD CONSTRAINT controls_policies_control_id_fkey
+    FOREIGN KEY (control_id)
+    REFERENCES controls(id)
+    ON DELETE CASCADE;
