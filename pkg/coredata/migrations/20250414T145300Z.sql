7f1d6322da56cba245206b4d095392dad143cc9c
gearnode <bryan@frimin.fr>
2025-04-15 20:53:15 -0700

    Fix migration
    Signed-off-by: gearnode <bryan@frimin.fr>

--- pkg/coredata/migrations/20250414T145300Z.sql
+++ pkg/coredata/migrations/20250414T145300Z.sql
@@ -1,5 +1,18 @@
-DELETE FROM controls WHERE framework_id NOT IN (SELECT id FROM frameworks);
 ALTER TABLE controls DROP CONSTRAINT IF EXISTS controls_framework_id_fkey;
+ALTER TABLE controls_mesures DROP CONSTRAINT IF EXISTS controls_mesures_control_id_fkey;
+ALTER TABLE controls_policies DROP CONSTRAINT IF EXISTS controls_policies_control_id_fkey;
+
+ALTER TABLE controls_mesures
+    ADD CONSTRAINT controls_mesures_control_id_fkey
+    FOREIGN KEY (control_id)
+    REFERENCES controls(id)
+    ON DELETE CASCADE;
+
+ALTER TABLE controls_policies
+    ADD CONSTRAINT controls_policies_control_id_fkey
+    FOREIGN KEY (control_id)
+    REFERENCES controls(id)
+    ON DELETE CASCADE;
 
 ALTER TABLE controls
     ADD CONSTRAINT controls_framework_id_fkey
@@ -7,16 +20,4 @@ ALTER TABLE controls
     REFERENCES frameworks(id)
     ON DELETE CASCADE;
 
-ALTER TABLE controls_mesures DROP CONSTRAINT IF EXISTS controls_mesures_control_id_fkey;
-ALTER TABLE controls_mesures
-    ADD CONSTRAINT controls_mesures_control_id_fkey
-    FOREIGN KEY (control_id)
-    REFERENCES controls(id)
-    ON DELETE CASCADE;
-
-ALTER TABLE controls_policies DROP CONSTRAINT IF EXISTS controls_policies_control_id_fkey;
-ALTER TABLE controls_policies
-    ADD CONSTRAINT controls_policies_control_id_fkey
-    FOREIGN KEY (control_id)
-    REFERENCES controls(id)
-    ON DELETE CASCADE;
+DELETE FROM controls WHERE framework_id NOT IN (SELECT id FROM frameworks);
