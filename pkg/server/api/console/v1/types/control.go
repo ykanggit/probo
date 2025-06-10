e0a575f9845429a1539d54d39b465c41251cb92e
Bryan Frimin <bryan@getprobo.com>
2025-06-09 21:20:58 -0700

    Style
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- pkg/server/api/console/v1/types/control.go
+++ pkg/server/api/console/v1/types/control.go
@@ -22,6 +22,16 @@ import (
 
 type (
 	ControlOrderBy OrderBy[coredata.ControlOrderField]
+
+	ControlConnection struct {
+		TotalCount int
+		Edges      []*ControlEdge
+		PageInfo   PageInfo
+
+		Resolver any
+		ParentID gid.GID
+		Filters  *coredata.ControlFilter
+	}
 )
 
 func NewControlConnection(
