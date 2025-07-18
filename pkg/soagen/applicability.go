c9b2fdb908dd89d0097a51af96f84116f9c870bb
Bryan Frimin <bryan@getprobo.com>
2025-07-18 16:38:42 +0200

    Fix color
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- pkg/soagen/applicability.go
+++ pkg/soagen/applicability.go
@@ -33,19 +33,19 @@ func (a Applicability) String() string {
 }
 
 func (a Applicability) MarshalExcel() ExcelValue {
-	color := "#FFB6C1" // Light red for not applicable
+	color := "FFB6C1" // Light red for not applicable
 	if a.IsApplicable {
-		color = "#90EE90" // Light green for applicable
+		color = "90EE90" // Light green for applicable
 	}
 
 	return ExcelValue{
 		Value: a.Value,
 		Style: &excelize.Style{
 			Border: []excelize.Border{
-				{Type: "left", Color: "#000000", Style: 1},
-				{Type: "top", Color: "#000000", Style: 1},
-				{Type: "bottom", Color: "#000000", Style: 1},
-				{Type: "right", Color: "#000000", Style: 1},
+				{Type: "left", Color: "000000", Style: 1},
+				{Type: "top", Color: "000000", Style: 1},
+				{Type: "bottom", Color: "000000", Style: 1},
+				{Type: "right", Color: "000000", Style: 1},
 			},
 			Fill:      excelize.Fill{Type: "pattern", Color: []string{color}, Pattern: 1},
 			Alignment: &excelize.Alignment{Horizontal: "center", Vertical: "center", WrapText: true},
