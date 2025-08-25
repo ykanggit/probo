65000082a07a96eefd770329cfaf025346ce8cd8
Yong Kang <yong.kang@kaamel.com>
2025-08-25 14:03:27 -0700

    change the color and title

--- packages/hooks/src/usePageTitle.ts
+++ packages/hooks/src/usePageTitle.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export function usePageTitle(title: string) {
     useEffect(() => {
-        document.title = title + " - Probo";
+        document.title = title;
         return () => {
             document.title = "Probo";
         };
